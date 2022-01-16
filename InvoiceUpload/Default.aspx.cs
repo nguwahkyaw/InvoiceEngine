119db9573c18eb82f25a94ea9c8f7dbaaf4e3eb4
Ngu Wah Kyaw <nguwahkyaw5@gmail.com>
2022-01-16 10:19:35 +0630

    CSV upload baseline

--- InvoiceUpload/Default.aspx.cs
+++ InvoiceUpload/Default.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace InvoiceUpload
 {
@@ -12,6 +16,51 @@ namespace InvoiceUpload
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Upload(object sender, EventArgs e)
+        {
+            //Upload and save the file.
+            string csvPath = Server.MapPath("~/Files/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
+            FileUpload1.SaveAs(csvPath);
+
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[5] 
+                { new DataColumn("TransactionID", typeof(string)),
+                new DataColumn("Amount", typeof(decimal)),
+                new DataColumn("Currency",typeof(string)),
+                new DataColumn("TransactionDate",typeof(DateTime)),
+                new DataColumn("Status",typeof(string))
+                }
+                );
+
+
+            string csvData = File.ReadAllText(csvPath);
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+
+            string consString = ConfigurationManager.ConnectionStrings["strConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(consString))
+            {
+                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                {
+                    //Set the database table name.
+                    sqlBulkCopy.DestinationTableName = "dbo.Invoice";
+                    con.Open();
+                    sqlBulkCopy.WriteToServer(dt);
+                    con.Close();
+                }
+            }
         }
     }
 }
